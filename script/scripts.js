02ded4584340ededc6d801b5d4f54f7d39684966
Gabriel Novaga Pereira <gnovaga@hotmail.com>
2026-06-04 15:21:13 -0300

    funcao para seguir pro proximo slide e retornar

--- script/scripts.js
+++ script/scripts.js
@@ -6,3 +6,18 @@ let index = 0;
 const nextBtn = document.getElementById("next-btn");
 const prevBtn = document.getElementById("prev-btn");
 const chooseBtn = document.getElementById("choose-btn");
+
+const showSlide = (nextIndex) => {
+    slides[index].classList.remove("active");
+    index = newIndex;
+
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+
+    if (index >= slides.length) {
+        index = 0;
+    }
+
+    slides[index].classList.add("active");
+}

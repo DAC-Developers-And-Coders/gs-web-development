e4670897fe49203d1335174ff45d98127c53e217
Gabriel Novaga Pereira <gnovaga@hotmail.com>
2026-06-04 15:17:34 -0300

    criando a pasta javascript e constantes

---
+++ script/scripts.js
@@ -0,0 +1,8 @@
+const slides = document.querySelectorAll(".card-objective-item");
+
+let index = 0;
+
+
+const nextBtn = document.getElementById("next-btn");
+const prevBtn = document.getElementById("prev-btn");
+const chooseBtn = document.getElementById("choose-btn");
